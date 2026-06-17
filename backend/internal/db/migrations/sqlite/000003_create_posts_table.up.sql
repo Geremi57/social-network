4664b069677c5ce95f55191c448397e446137571
geremi57 <wangageremi725@gmail.com>
2026-06-17 17:54:22 +0300

    add author name in post table

---
+++ backend/internal/db/migrations/sqlite/000003_create_posts_table.up.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    author_name TEXT NOT NULL,
+    content TEXT NOT NULL,
+    image_path TEXT,
+    privacy TEXT NOT NULL DEFAULT 'public' CHECK(privacy IN ('public', 'almost_private', 'private')),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER NOT NULL,
+    author_name TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    image_path TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS post_viewers (
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY (post_id, user_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
