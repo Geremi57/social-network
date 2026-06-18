4e2f4b4f5208b55840286770a05b570f7e0f68ab
geremi57 <wangageremi725@gmail.com>
2026-06-18 15:38:03 +0300

    add date of birth avatar and about me and nickname properties to the users table

--- backend/internal/db/migrations/sqlite/000001_create_users_table.up.sql
+++ backend/internal/db/migrations/sqlite/000001_create_users_table.up.sql
@@ -3,6 +3,10 @@ CREATE TABLE IF NOT EXISTS users(
 		firstname TEXT NOT NULL,
 		lastname TEXT NOT NULL,
 		password_hash TEXT NOT NULL,
+		date_of_birth DATE NOT NULL,
 		email TEXT NOT NULL UNIQUE,
+		avatar TEXT,
+		about_me TEXT,
+		nickname TEXT,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 		);
