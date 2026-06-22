dc46f162d42c7ac7eab2c50ceab8f8602a6844a6
elvisotieno0 <elvisotienomboya@gmail.com>
2026-06-22 16:47:01 +0300

    its better when that func stays in the models though

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -166,32 +166,3 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	// fmt.Println(hashedPassword)
 }
-
-func userRow(id int) map[string]any {
-	var firstName, lastName, email, dob string
-	var nickname, aboutMe, avatarPath sql.NullString
-	var isPublic int
-	db.DB.QueryRow(`
-		SELECT firstname, lastname, email, date_of_birth, nickname, about_me, avatar_path, is_public
-		FROM users WHERE id = ?
-	`, id).Scan(&firstName, &lastName, &email, &dob, &nickname, &aboutMe, &avatarPath, &isPublic)
-
-	user := map[string]any{
-		"id":            id,
-		"firstname":    firstName,
-		"lastname":     lastName,
-		"email":         email,
-		"date_of_birth": dob,
-		"is_public":     isPublic == 1,
-	}
-	if nickname.Valid {
-		user["nickname"] = nickname.String
-	}
-	if aboutMe.Valid {
-		user["about_me"] = aboutMe.String
-	}
-	if avatarPath.Valid {
-		user["avatar_path"] = avatarPath.String
-	}
-	return user
-}
