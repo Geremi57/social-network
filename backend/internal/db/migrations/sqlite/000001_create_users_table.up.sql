629f536105d75c865c7f7416d7a12b8434cc8cd0
elvisotieno0 <elvisotienomboya@gmail.com>
2026-06-22 16:26:28 +0300

    added a param for public profile, also added avatar_text, not sure if its the same with avatar

--- backend/internal/db/migrations/sqlite/000001_create_users_table.up.sql
+++ backend/internal/db/migrations/sqlite/000001_create_users_table.up.sql
@@ -8,5 +8,7 @@ CREATE TABLE IF NOT EXISTS users(
 		avatar TEXT,
 		about_me TEXT,
 		nickname TEXT,
+		avatar_path TEXT,
+		is_public INTEGER NOT NULL DEFAULT 0,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 		);
